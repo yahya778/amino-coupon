7d03b8b821503ad93f6b992fd7808cebde8e25ea
yahya778 <82583237+yahya778@users.noreply.github.com>
2021-04-15 14:12:47 +0300

    Add files via upload

--- deviceid.py
+++ deviceid.py
@@ -1,33 +1,8 @@
+#!/usr/bin/env python3
+import os
 
-import discord
-from discord.ext import commands
-from discord.ext import tasks
+device=input("hallo give me your \nparallel space device id : ")
 
-bot = commands.Bot(command_prefix='!')
-
-
-@bot.command()
-async def open1(ctx):
-	embed=discord.Embed(title="صفحة القران 1")
-	embed.set_image(url="https://i.ibb.co/Jz77x4s/001.gif")
-	embed.set_footer(text="بوت القران")
-	await ctx.send(embed=embed)
-
-@bot.command()
-async def open3(ctx):
-	embed=discord.Embed(title="صفحة القران3")
-	embed.set_image(url="https://i.ibb.co/hf7cC1f/page-003.jpg")
-	embed.set_footer(text="بوت القران")
-	await ctx.send(embed=embed)
-
-
-
-@bot.command()
-async def open2(ctx):
-	embed=discord.Embed(title="صفحة القران 2")
-	embed.set_image(url="https://i.ibb.co/8BFt5ck/images-9.jpg")
-	embed.set_footer(text="بوت القران")
-	await ctx.send(embed=embed)
-
-
-bot.run("ODI2ODc4NjMzMDcwNDI4MTkw.YGS4vA.a_QxXiSbizA_PG2PcvYyMjXt7mk")
+ff='{\\"device_id\\": \\"'+device+'\\", \\"device_id_sig\\": \\"AaauX/ZA2gM3ozqk1U5j6ek89SMu\\", \\"user_agent\\": \\"Dalvik/2.1.0 (Linux; U; Android 7.1; LG-UK495 Build/MRA58K; com.narvii.amino.master/3.3.33180)\\"}'
+os.system("printf \""+ff+"\">device.json")
+os._exit(1)
